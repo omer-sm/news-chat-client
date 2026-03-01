d4c49e28a683b70ed72aa356b6e20815d5321b95
omer_sm <smorodinskyomer@gmail.com>
2026-03-01 17:59:32 +0200

    config: static page

--- vite.config.ts
+++ vite.config.ts
@@ -22,6 +22,7 @@ errorOnDuplicatesPkgDeps(devDependencies, dependencies);
  */
 export default defineConfig(({ command, mode }): UserConfig => {
   return {
+     base: '/news-chat-client/',
     plugins: [tailwindcss(), qwikCity(), qwikVite(), tsconfigPaths({ root: "." })],
     // This tells Vite which dependencies to pre-build in dev mode.
     optimizeDeps: {
